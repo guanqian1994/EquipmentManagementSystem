1634aa696a1b6301ba8bd0abd658f07f9159a17c
wslongchen@vip.qq.com <wslongchen@vip.qq.com>
2014-10-27 23:39:07 +0800

    MrPan

--- src/mainwindow.cpp
+++ src/mainwindow.cpp
@@ -10,25 +10,53 @@ MainWindow::MainWindow(QWidget *parent) :
     ui->setupUi(this);
 	
 	//TableWiget
-	ui->tableWidget->setColumnCount(10);
-	list << "Name" << "Description" << "RegistrationDate" << "RegistrationOperator" << "Value" << "LendPrice" << "IsLending" << "RecentLendRecord" << "Image" << "Remark";
+	ui->tableWidget->setColumnCount(11);
+	auto li = Database::get().getEquipmentList(0, 1000);
+	ui->tableWidget->setRowCount(li.size());
+	//let the line number hidden
+	ui->tableWidget->verticalHeader()->setVisible(false);
+	/*QHeaderView* hearderView = ui->tableWidget->verticalHeader();
+	hearderView->setHidden(true);*/
+	list << "Id" << "Name" << "Description" << "RegistDate" << "RegistOperator" << "Value" << "LendPrice" << "IsLending" << "RecentLendRecord" << "Image" << "Remark";
 	ui->tableWidget->setHorizontalHeaderLabels(list);
-	ui->tableWidget->horizontalHeader()->setStretchLastSection(9);
-	/*auto li = Database::get().getEquipmentList(0, 1000);
-	for (int i = 0;li.size(); i++)
+	ui->tableWidget->horizontalHeader()->setStretchLastSection(11);
+	ui->tableWidget->setSelectionBehavior(QAbstractItemView::SelectRows);
+	/*
+	auto li = Database::get().getEquipmentList(0, 1000);
+	ui->tableWidget->setRowCount(li.size());
+	for (int i = 0;i<li.size(); i++)
 	{
-		ui->tableWidget->setItem(i, 0, new QTableWidgetItem(li[i]._name));
-		ui->tableWidget->setItem(i, 1, new QTableWidgetItem(li[i]._description));
-		ui->tableWidget->setItem(i, 2, new QTableWidgetItem(li[i]._registrationDate));
-		ui->tableWidget->setItem(i, 3, new QTableWidgetItem(li[i]._registrationOperator));
-		ui->tableWidget->setItem(i, 4, new QTableWidgetItem(li[i]._value));
-		ui->tableWidget->setItem(i, 5, new QTableWidgetItem(li[i]._lendPrice));
-		ui->tableWidget->setItem(i, 6, new QTableWidgetItem(li[i]._isLending));
-		ui->tableWidget->setItem(i, 7, new QTableWidgetItem(li[i]._recentLendRecord));
-		ui->tableWidget->setItem(i, 8, new QTableWidgetItem(""));
-		ui->tableWidget->setItem(i, 9, new QTableWidgetItem(li[i]._remark));
-		
-	}*/
+		ui->tableWidget->setItem(i, 0, new QTableWidgetItem());
+		ui->tableWidget->setItem(i, 1, new QTableWidgetItem(li[i]._name));
+		ui->tableWidget->setItem(i, 2, new QTableWidgetItem(li[i]._description));
+		ui->tableWidget->setItem(i, 3, new QTableWidgetItem(li[i]._registrationDate));
+		ui->tableWidget->setItem(i, 4, new QTableWidgetItem(li[i]._registrationOperator));
+		ui->tableWidget->setItem(i, 5, new QTableWidgetItem(li[i]._value));
+		ui->tableWidget->setItem(i, 6, new QTableWidgetItem(li[i]._lendPrice));
+		ui->tableWidget->setItem(i, 7, new QTableWidgetItem(li[i]._isLending));
+		ui->tableWidget->setItem(i, 8, new QTableWidgetItem(li[i]._recentLendRecord));
+		ui->tableWidget->setItem(i, 8, new QTableWidgetItem(QIcon(""),""));
+		ui->tableWidget->setItem(0, 10, new QTableWidgetItem(li[0]._remark));
+	}
+	*/
+	
+	for (int i = 0; i <Database::get().getEquipmentCount(); i++)
+	{
+		for (auto& a : Database::get().getEquipmentList(0, 1000))
+		{
+			ui->tableWidget->setItem(i, 0, new QTableWidgetItem(a._id));
+			ui->tableWidget->setItem(i, 1, new QTableWidgetItem(a._name));
+			ui->tableWidget->setItem(i, 2, new QTableWidgetItem(a._description));
+			ui->tableWidget->setItem(i, 3, new QTableWidgetItem(a._registrationDate));
+			ui->tableWidget->setItem(i, 4, new QTableWidgetItem(a._registrationOperator));
+			ui->tableWidget->setItem(i, 5, new QTableWidgetItem(a._value));
+			ui->tableWidget->setItem(i, 6, new QTableWidgetItem(a._lendPrice));
+			ui->tableWidget->setItem(i, 7, new QTableWidgetItem(a._isLending));
+			ui->tableWidget->setItem(i, 8, new QTableWidgetItem(a._recentLendRecord));
+			ui->tableWidget->setItem(i, 8, new QTableWidgetItem(QIcon(""), ""));
+			ui->tableWidget->setItem(0, 10, new QTableWidgetItem(a._remark));
+		}
+	}
 }
 
 MainWindow::~MainWindow()
